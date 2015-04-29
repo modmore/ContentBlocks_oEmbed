739b8d41c07c7ebe9741085617e45e756f978944
Mark Hamstra <hello@markhamstra.com>
2015-04-29 14:13:10 +0200

    1.1.0

--- core/components/oembedinput/lexicon/en/default.inc.php
+++ core/components/oembedinput/lexicon/en/default.inc.php
@@ -2,3 +2,4 @@
 $_lang['oembedinput'] = 'oEmbed';
 $_lang['oembedinput.description'] = 'Allows the user to paste in an oembed-enabled link, which is then magically turned into an embedded resource via noembed.com.';
 $_lang['oembedinput.unset_embed'] = 'Delete Embed';
+$_lang['contentblocks.oembedinput_template.description'] = "A template for the embedded link. Available placeholders: <code>[[+html]]</code>, <code>[[+title]]</code>, <code>[[+url]]</code>, <code>[[+provider_name]]</code> and more depending on the embed source.";
